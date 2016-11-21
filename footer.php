6d820cc7984e903aafda17a4bdfb55dd50c21289
niraj2695 <nirajbohra@gmail.com>
2016-11-21 20:21:32 +1100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,60 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class=" mainBackground">
+
+			<footer id="footer" class="trans"> 	
+				<ul id="mainList"> 
+					<li class="menu-blog"> 
+						<a title="Blog"  target="_blank" href="./blog.php"> Blog </a>
+					</li>
+					<li class="menu-faq"> 
+						<a  target="_blank" href="./faq.php"> FAQs </a>
+					</li>
+					<li class="menu-contact"> 
+						<a title="Contact"  target="_blank" href="./contact.php"> Contact </a>
+					</li>
+					<li class="menu-press"> 
+						<a title="Press"  target="_blank" href="./press.php"> Press </a>
+					</li>
+					<li class="menu-terms-privacy"> 
+						<a title="Terms & Privacy"  target="_blank" href="./privacy-policy.php"> terms &amp; privacy  </a>
+					</li>
+
+					<li class="nav navbar-right" style="background-color:#27DC94;">
+						
+						<a title="Coming Soon" href="#" style="color:#000;"><i class="fa fa-mobile fa-3x" aria-hidden="true" ></i>  <b style="position: relative;bottom:10px;">  	COMING SOON </b>
+						</a> 
+					</li> 
+					
+					<li class="nav navbar-right">  
+						<a href=""> 
+							<i class="fa fa-youtube-play fa-2x" aria-hidden="true"></i>
+						</a> 
+					</li>
+					<li class="nav navbar-right">   
+						<a href=""> 
+							<i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
+						</a> 
+					</li>
+					<li class="nav navbar-right">  
+						<a href=""> 
+							<i class="fa fa-twitter fa-2x" aria-hidden="true"></i>
+						</a> 
+					</li>
+					<li class="nav navbar-right">  
+						<a href=""> 
+							<i class="fa fa-facebook fa-2x" aria-hidden="true"></i>
+						</a> 
+					</li>
+					<li class="nav navbar-right">  
+						<a href=""> 
+							<i class="fa fa-envelope-o fa-2x"> </i>
+						</a> 
+					</li>
+
+					
+				</ul>
+			</footer>
+		</div>
+	</div>
+</div>
